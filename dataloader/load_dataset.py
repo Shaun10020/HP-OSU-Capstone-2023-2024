fd7b883b6dc2dc60736f452a120ecb235f72f04a
Shaun10020 <10020guava@gmail.com>
2024-01-20 09:25:42 -0800

    Update Dataset

--- dataloader/load_dataset.py
+++ dataloader/load_dataset.py
@@ -4,6 +4,7 @@ import os
 from config.config import features,labels,img_extension,height,width
 import cv2
 from PIL import Image
+from torch.utils.data import Dataset
 
 def read_input_image(transform,input,pdf_name,pn):
     img = []
@@ -12,19 +13,26 @@ def read_input_image(transform,input,pdf_name,pn):
         path = os.path.join(input,pdf_name,filenamme)
         img.append(transform(torchvision.io.read_image(path)))
     return torch.cat(img)
-    
-def load_dataset(input,label_folder,intermediate):
-    transform = torchvision.transforms.Resize((height,width),antialias=True)
-    trans2Tensor = torchvision.transforms.ToTensor()
-    dataset = []
-    for result in intermediate:
-        pdf_name = result['pdf_filename'].replace('.pdf','')
-        pn = str(result['page_num'])
-        while len(pn) <4:
-            pn = '0'+pn
-        data = {"input":read_input_image(transform,input,pdf_name,pn)}
-        for label in labels:
-            data[label] = trans2Tensor(Image.fromarray(cv2.imread(os.path.join(label_folder,pdf_name,result[label]))))
-        dataset.append(data)
-    return dataset
         
+        
+class SimplexDataset(Dataset):
+    def __init__(self, input,label_folder,intermediate, transform=None):   
+        if transform == None: 
+            transform = torchvision.transforms.Resize((height,width),antialias=True)
+        trans2Tensor = torchvision.transforms.ToTensor()
+        self.dataset = []
+        for result in intermediate:
+            pdf_name = result['pdf_filename'].replace('.pdf','')
+            pn = str(result['page_num'])
+            while len(pn) <4:
+                pn = '0'+pn
+            data = {"input":read_input_image(transform,input,pdf_name,pn)}
+            for label in labels:
+                data[label] = trans2Tensor(Image.fromarray(cv2.imread(os.path.join(label_folder,pdf_name,result[label]))))
+            self.dataset.append(data)
+            
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, index):
+        return self.dataset[index]
