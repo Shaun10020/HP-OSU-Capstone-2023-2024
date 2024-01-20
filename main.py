10bad916261c115c86da40efe7313c4eefd4ceca
Shaun10020 <10020guava@gmail.com>
2024-01-20 11:24:18 -0800

    Update Dataset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from config.args import get_arguments
-from dataloader.load_dataset import load_dataset
+from dataloader.load_dataset import SimplexDataset
 from utils.load_json import load_results
 from utils.check_data import checkResult
 
@@ -13,5 +13,5 @@ device = torch.device("cpu") if torch.cuda.is_available() else  torch.device("gp
 if __name__ == "__main__":
     pdf,algorithm,intermediate = load_results(args.label_folder)
     intermediate = checkResult(args.input_folder,args.label_folder,intermediate)
-    inputs = load_dataset(args.input_folder,args.label_folder,intermediate)
+    dataset = SimplexDataset(args.input_folder,args.label_folder,intermediate)
     
