87c73c77dc6f3ef4ed4bba9cfe0d2ba35a262c18
Shaun10020 <10020guava@gmail.com>
2024-01-24 00:28:58 -0800

    duplexDataset Template

--- dataloader/load_data.py
+++ dataloader/load_data.py
@@ -5,7 +5,7 @@ import cv2
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader, random_split
 
-from config.config import features,labels,img_extension,input_height,input_width,output_height,output_width,train_val_ratio,random_seed,input_label,pin_memory
+from config.config import features,labels,img_extension,input_height,input_width,output_height,output_width,train_val_ratio,random_seed,input_label,pin_memory,duplex_labels
 from utils.check_data import checkInput, checkLabel
 
 class SimplexDataset(Dataset):
@@ -32,6 +32,72 @@ class SimplexDataset(Dataset):
                     for label in labels:
                         data[label] = transform_output(trans2Tensor(Image.fromarray(cv2.imread(os.path.join(label_folder,pdf_name,page[label])))))
                     self.dataset.append(data)
+            
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, index):
+        return self.dataset[index]
+    
+class DuplexDataset(Dataset):
+    def __init__(self, input,label_folder,intermediate, transform=None,transform_output=None):   
+        if transform == None: 
+            transform = torchvision.transforms.Resize((input_height,input_width,),antialias=True)
+        if transform_output == None: 
+            transform_output = torchvision.transforms.Resize((output_height,output_width,),antialias=True)
+        trans2Tensor = torchvision.transforms.ToTensor()
+        self.dataset = []
+        for result in intermediate:
+            duplex = False
+            for page in result:
+                pdf_name = page['pdf_filename'].replace('.pdf','')
+                _pn = page['page_num']
+                pn = str(_pn)
+                while len(pn) <4:
+                    pn = '0'+pn
+                if checkInput(input,pdf_name,pn) and checkLabel(page,label_folder,pdf_name):
+                    if duplex:
+                        duplex = False
+                        if next != _pn:
+                            continue
+                        for feature in features:
+                            filenamme = feature+"-"+str(pn)+"-grayscale"+img_extension
+                            path = os.path.join(input,pdf_name,filenamme)
+                            img.append(transform(torchvision.io.read_image(path)))
+                        data[input_label] = torch.cat(img)
+                        for label in labels:
+                            data[label+"_2"] = transform_output(trans2Tensor(Image.fromarray(cv2.imread(os.path.join(label_folder,pdf_name,page[label])))))
+                        self.dataset.append(data)
+                    elif duplex_labels[0] in page:
+                        duplex = True
+                        next = _pn + 1
+                        img = []
+                        data = {}
+                        for feature in features:
+                            filenamme = feature+"-"+str(pn)+"-grayscale"+img_extension
+                            path = os.path.join(input,pdf_name,filenamme)
+                            img.append(transform(torchvision.io.read_image(path)))
+                        for label in labels:
+                            data[label+"_1"] = transform_output(trans2Tensor(Image.fromarray(cv2.imread(os.path.join(label_folder,pdf_name,page[label])))))
+                        for label in duplex_labels:
+                            data[label] = transform_output(trans2Tensor(Image.fromarray(cv2.imread(os.path.join(label_folder,pdf_name,page[label])))))
+                    # elif _pn%2:
+                    #     duplex = False
+                    #     img = []
+                    #     for feature in features:
+                    #         filenamme = feature+"-"+str(pn)+"-grayscale"+img_extension
+                    #         path = os.path.join(input,pdf_name,filenamme)
+                    #         img.append(transform(torchvision.io.read_image(path)))
+                    #     for _ in features:
+                    #         img.append(transform(torch.zeros(1)))
+                    #     data = {input_label:torch.cat(img)}
+                    #     for label in labels:
+                    #         data[label+"_1"] = transform_output(trans2Tensor(Image.fromarray(cv2.imread(os.path.join(label_folder,pdf_name,page[label])))))
+                    #     for label in labels:
+                    #         data[label+"_2"] = transform_output(torch.zeros(1))
+                    #     for label in duplex_labels:
+                    #         data[label] = transform_output(torch.zeros(1))
+                    #     self.dataset.append(data)
             
     def __len__(self):
         return len(self.dataset)
