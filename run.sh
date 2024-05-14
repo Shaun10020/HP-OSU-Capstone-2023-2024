b2d08fb676118a2cc2ed4765efaf9fc890128854
Shaun10020 <10020guava@gmail.com>
2024-05-14 11:24:28 -0700

    update bash script

--- run.sh
+++ run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# python main.py --model=unet --mode=train --batch=5 --epoch=70 --dataset=simplex
-# python main.py --model=unet --mode=train --batch=5 --epoch=70 --dataset=duplex
-# python main.py --model=enet --mode=train --batch=5 --epoch=70 --dataset=simplex
-# python main.py --model=enet --mode=train --batch=5 --epoch=120 --dataset=duplex 
+## Train the models
+python main.py --model=unet --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
+python main.py --model=enet --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
+python main.py --model=deeplabv3 --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
+python main.py --model=deeplabv3plus --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
 
-# python main.py --model=unet --mode=test --batch=5 --dataset=simplex
-# python main.py --model=unet --mode=test --batch=5 --dataset=duplex
-# python main.py --model=enet --mode=test --batch=5 --dataset=simplex
-# python main.py --model=enet --mode=test --batch=5 --dataset=duplex --parallel=False
-python main.py --model=enet --mode=test --batch=5 --dataset=duplex --parallel=True
-# python main.py --model=deeplabv3 --mode=test --batch=5 --dataset=duplex
-# python main.py --model=deeplabv3+ --mode=test --batch=5 --dataset=duplex
+## Test models with GPU
+python main.py --model=unet --mode=test --batch=8 --dataset=duplex
+python main.py --model=enet --mode=test --batch=8 --dataset=duplex
+python main.py --model=deeplabv3 --mode=test --batch=8 --dataset=duplex
+python main.py --model=deeplabv3+ --mode=test --batch=8 --dataset=duplex
+
+## Test models with CPU
+python main.py --model=unet --mode=test --batch=8 --dataset=duplex --device=cpu
+python main.py --model=enet --mode=test --batch=8 --dataset=duplex --device=cpu
+python main.py --model=deeplabv3 --mode=test --batch=8 --dataset=duplex --device=cpu
+python main.py --model=deeplabv3+ --mode=test --batch=8 --dataset=duplex --device=cpu
+
+## Test models with CPU and parallel processes
+python main.py --model=unet --mode=test --batch=8 --dataset=duplex --device=cpu --parallel=True
+python main.py --model=enet --mode=test --batch=8 --dataset=duplex --device=cpu --parallel=True
+python main.py --model=deeplabv3 --mode=test --batch=8 --dataset=duplex --device=cpu --parallel=True
+python main.py --model=deeplabv3+ --mode=test --batch=8 --dataset=duplex --device=cpu --parallel=True
