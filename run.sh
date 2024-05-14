47e198973fd784accf3949c9bb59d0f43e1f8825
Shaun10020 <10020guava@gmail.com>
2024-05-14 11:29:16 -0700

    fix typo

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 python main.py --model=unet --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
 python main.py --model=enet --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
 python main.py --model=deeplabv3 --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
-python main.py --model=deeplabv3plus --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
+python main.py --model=deeplabv3+ --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-3 --lr_decay=0.1 --lr_epoch=40
 
 ## Test models with GPU
 python main.py --model=unet --mode=test --batch=8 --dataset=duplex
