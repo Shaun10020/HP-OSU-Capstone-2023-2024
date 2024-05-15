d50ba4c7b21b89d802b1470580f831a13840f9bb
Shaun10020 <10020guava@gmail.com>
2024-05-14 17:21:48 -0700

    update hyperparameters again

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ## Train the models
-python main.py --model=unet --mode=train --batch=8 --epoch=80 --dataset=duplex --lr=1e-3 --lr_decay=0.5 --lr_epoch=25
-python main.py --model=enet --mode=train --batch=8 --epoch=80 --dataset=duplex --lr=1e-3 --lr_decay=0.5 --lr_epoch=25
-python main.py --model=deeplabv3 --mode=train --batch=8 --epoch=80 --dataset=duplex --lr=1e-3 --lr_decay=0.5 --lr_epoch=25
-python main.py --model=deeplabv3+ --mode=train --batch=8 --epoch=80 --dataset=duplex --lr=1e-3 --lr_decay=0.5 --lr_epoch=25
+python main.py --model=unet --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-4 --lr_decay=0.5 --lr_epoch=30
+python main.py --model=enet --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-4 --lr_decay=0.5 --lr_epoch=30
+python main.py --model=deeplabv3 --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-4 --lr_decay=0.5 --lr_epoch=30
+python main.py --model=deeplabv3+ --mode=train --batch=8 --epoch=120 --dataset=duplex --lr=1e-4 --lr_decay=0.5 --lr_epoch=30
 
 ## Test models with GPU
 python main.py --model=unet --mode=test --batch=8 --dataset=duplex
